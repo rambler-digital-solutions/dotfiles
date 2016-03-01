3d72475b9c5fd9daec0d814b3365ae1d96dd0eaf
Chernobrov Mike <m.chernobrov@rambler-co.ru>
2016-03-01 13:55:01 +0300

    add test and travis ci integration

---
+++ test/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Depends
+var chai = require('chai');
+var exporter = require('../index.js');
+
+describe('eslint.index', function () {
+  var expect = chai.expect;
+
+  it('function should return object', function() {
+    expect(exporter).to.be.an('object');
+  });
+
+  it('function should contain `parser` attribute', function() {
+    expect(exporter).have.an.property('parser').is.equal('babel-eslint');
+  });
+
+  it('function should contain `ecmaFeatures` attribute', function() {
+    expect(exporter).have.an.property('ecmaFeatures');
+  });
+
+  it('function should contain `rules` attribute', function() {
+    expect(exporter).have.an.property('rules');
+  });
+});
