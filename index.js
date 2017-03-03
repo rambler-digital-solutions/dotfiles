e7d91054bcbda86d715aedfa6fce47bf489e36cc
an.smirnov <sandark7@gmail.com>
2017-03-03 11:36:21 +0300

    feature/eslint-update Added rules for spacing in braces

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 
 // you could do this all at once if you wanted to look cool
 var filename = path.join(__dirname, '.eslintrc');
-var data = fs.readFileSync(filename, {encoding: 'utf-8'});
+var data = fs.readFileSync(filename, { encoding: 'utf-8' });
 var dataWithoutComments = stripComments(data);
 var parsed = JSON.parse(dataWithoutComments);
 
